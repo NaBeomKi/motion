aaa2bd61fd2d33a9429979a031a52a5f235a2802
nabeomki <beomki69@gmail.com>
2021-12-13 14:35:12 +0900

    fix: 정규표현식으로 입력되는 url형태에 관계없이 id를 추출해 임베드 링크를 생성하도록 수정

--- src/components/page/item/Video.ts
+++ src/components/page/item/Video.ts
@@ -22,11 +22,23 @@ export default class VideoComponent extends BaseComponent<HTMLElement> {
     const $iframe = this.$element.querySelector(
       ".video__iframe"
     )! as HTMLIFrameElement;
-    $iframe.src = url;
+    $iframe.src = this.convertToEmbeddedURL(url);
 
     const $title = this.$element.querySelector(
       ".video__title"
     )! as HTMLParagraphElement;
     $title.textContent = title;
+  }
+
+  private convertToEmbeddedURL(url: string): string {
+    // 정규표현식 Regex
+    // https://regexr.com/5l6nr
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9(-|_)]{11}))|(?:youtu.be\/([a-zA-Z0-9(-|_)]{11})))/;
+    const match = url.match(regExp);
+    const videoId = match ? match[1] || match[2] : undefined;
+
+    if (videoId) return `https://www.youtube.com/embed/${videoId}`;
+    return url;
   }
 }
